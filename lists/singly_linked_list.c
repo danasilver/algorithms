35877600d562308547b9b66982726af7ac66b6cf
danasilver <dsilver1221@gmail.com>
2014-03-02 17:17:35 -0500

    Add find and next methods on the singly linked list.

--- lists/singly_linked_list.c
+++ lists/singly_linked_list.c
@@ -12,6 +12,7 @@ typedef struct Node {
   struct Node* next;
 } Node;
 
+/* Print a singly linked list. */
 void print_list(Node* root) {
   while (root) {
     printf("%i ", root->data);
@@ -20,6 +21,7 @@ void print_list(Node* root) {
   printf("\n");
 }
 
+/* Reverse the order of a singly linked list. */
 Node* reverse(Node* root) {
   Node* new_root = 0;
   while (root) {
@@ -31,19 +33,36 @@ Node* reverse(Node* root) {
   return new_root;
 }
 
+/* Insert a node at the head of the singly linked list. */
 Node* insert(Node* root, int data) {
-  Node* new_root;
-  new_root = (Node*)malloc(sizeof(Node));
+  Node* new_root = (Node*)malloc(sizeof(Node));
   new_root->data = data;
   new_root->next = root;
 
   return new_root;
 }
 
+/* Return the next node in a singly linked list. */
+Node* next(Node* node) {
+  return node->next;
+}
+
+/* Find a node in the singly linked list or return null. */
+Node* find(Node* root, int key) {
+  while (root) {
+    if (root->data == key) {
+      return root;
+    }
+    else {
+      root = root->next;
+    }
+  }
+  return NULL;
+}
+
 int main() {
   Node* list;
   list = (Node*)malloc(sizeof(Node));
-
 
   list->data = 23;
   list = insert(list, 24);
@@ -52,6 +71,14 @@ int main() {
 
   list = reverse(list);
   print_list(list);
+
+  Node* exists = find(list, 23);
+  if (exists != NULL) {
+    printf("%i exists!\n", exists->data);
+    printf("%i is the data on the next node.\n", next(exists)->data);
+  } else {
+    printf("%i doesn't exist!\n", 23);
+  }
 
   return 0;
 }
